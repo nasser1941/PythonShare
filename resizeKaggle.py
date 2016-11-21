868a8837440bd88cc46977a648edea6c17f3feca
Naser Derakhshan <naser.derakhshan@polimi.it>
2016-11-21 17:55:29 +0100

    Latest update of the project

--- resizeKaggle.py
+++ resizeKaggle.py
@@ -1,10 +1,14 @@
 import cv2
 import os
 
-dirAddress = 'train/train/SHARK'
-dirAddressResize = 'train/train/SHARK/resize'
+dirAddress = 'train/train/YFT'
+dirAddressResize = 'train/train/YFT/resize'
 for filename in os.listdir(dirAddress):
         img = cv2.imread(os.path.join(dirAddress, filename))
-        res = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(os.path.join(dirAddressResize, filename), res)
-        print 'Image: ' + filename + ' resized and saved'
+        if img==None:
+          continue
+        height, width, channels = img.shape
+        if (height>0 and width>0):
+                res = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
+                cv2.imwrite(os.path.join(dirAddressResize, filename), res)
+                print 'Image: ' + filename + ' saved'
